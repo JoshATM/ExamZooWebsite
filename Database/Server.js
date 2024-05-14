54ac469c58102f65f56bd7761b1be895fce4ab8e
JoshATM <joshua.downes.747@accesstomusic.ac.uk>
2024-05-14 13:51:49 +0100

    Updated

--- Database/Server.js
+++ Database/Server.js
@@ -1,8 +1,9 @@
 // Importing Modules
 import express from "express"
-import mysql from "mysql";
 import cors from "cors";
 import bodyParser from "body-parser";
+import express from "express";
+import pg from "pg";
 
 // Front-end Server Port
 const frontend = "http://localhost:5173"
@@ -16,213 +17,177 @@ app.use(
   })
 )
 
-// Database Connection
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Turkish-snooze-tumbrel-reagent-aster-photo-hecatomb-impure-sedate",
-  database: "rza_db",
-})
 
+// Database Connection
+const pool = new pg.Pool({
+    user: "Josh",
+    password: "",
+    host: "your_host",
+    port: "your_port",
+    database: "exam-zoo-website-db",
+});
 
 // Whether or not the database has been connected to successfully
-db.connect((err) => {
+pool.connect((err) => {
   if (err) {
     console.error("Error connecting to database: " + err.stack);
     return;
   }
   console.log("Connected to database");
-})
-
+});
 
 // Server Running on Port
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
-
-
-
+});
 
 // Table for Users
 const usersTable = `
-  CREATE TABLE IF NOT EXISTS Users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(255),
-    LastName VARCHAR(255),
-    Email VARCHAR(255),
-    Password VARCHAR(255)
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
   )
 `;
 
-db.query(usersTable, (err) => {
+pool.query(usersTable, (err) => {
   if (err) throw err;
   console.log('Users table created');
 });
-
-
 
 // Sign Up
 app.post("/register", (req, res) => {
   const { firstname, lastname, email, password } = req.body;
 
   const query =
-    "INSERT INTO users (firstname, lastname, email, password) VALUES (?, ?, ?, ?)";
-    db.query(
-    query,
-    [firstname, lastname, email, password],
-    (err) => {
-      if (err) {
-        console.error("Error registering user: " + err.stack);
-        res.status(500).send("Error registering user");
-        return;
-      }
-
-      res.status(200).send("User registered successfully");
+    "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)";
+  pool.query(query, [firstname, lastname, email, password], (err) => {
+    if (err) {
+      console.error("Error registering user: " + err.stack);
+      res.status(500).send("Error registering user");
+      return;
     }
-  )
-})
+
+    res.status(200).send("User registered successfully");
+  });
+});
 
 // Log In
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  const query = "SELECT * FROM users WHERE email = ? AND password = ?";
-  db.query(query, [email, password], (err, results) => {
+  const query = "SELECT * FROM users WHERE email = $1 AND password = $2";
+  pool.query(query, [email, password], (err, results) => {
     if (err) {
       console.error("Error logging in: " + err.stack);
       res.status(500).send("Error logging in");
       return;
     }
 
-    if (results.length === 0) {
+    if (results.rows.length === 0) {
       res.status(401).send("Invalid email or password");
       return;
     }
 
     res.status(200).send("Login successful");
-  })
-})
-
-
-
+  });
+});
 
 // Sign Up email validation
 app.get("/check-email/:email", (req, res) => {
   const { email } = req.params;
-  const query = "SELECT * FROM Users WHERE Email = ?";
-  db.query(query, [email], (err, results) => {
+  const query = "SELECT * FROM users WHERE email = $1";
+  pool.query(query, [email], (err, results) => {
     if (err) {
       console.error("Error checking email: " + err.stack);
       res.status(500).send("Error checking email");
       return;
     }
 
-    const emailExists = results.length > 0;
+    const emailExists = results.rows.length > 0;
     res.json(emailExists);
-  })
-})
-
-
-
-
-
+  });
+});
 
 // Incomplete Dashboard
 
 // Would get the users name and email if finished
-app.get("/users", (res) => {
-  const query = "SELECT * FROM Users";
-  db.query(query, (err, results) => {
+app.get("/users", (req, res) => {
+  const query = "SELECT * FROM users";
+  pool.query(query, (err, results) => {
     if (err) {
       console.error("Error fetching users: " + err.stack);
       res.status(500).send("Error fetching users");
       return;
     }
 
-    res.json(results);
-  })
-})
-
-
-
+    res.json(results.rows);
+  });
+});
 
 // Would get the users reservations if finished
-app.get("/reservations", (res) => {
-  const query = "SELECT * FROM HotelReservations";
-  db.query(query, (err, results) => {
+app.get("/reservations", (req, res) => {
+  const query = "SELECT * FROM hotel_reservations";
+  pool.query(query, (err, results) => {
     if (err) {
       console.error("Error fetching hotel reservations: " + err.stack);
       res.status(500).send("Error fetching hotel reservations");
       return;
     }
 
-    res.json(results);
-  })
-})
-
-
-
+    res.json(results.rows);
+  });
+});
 
 // Would get the users bookings if finished
-app.get("/bookings", (res) => {
-  const query = "SELECT * FROM Bookings";
-  db.query(query, (err, results) => {
+app.get("/bookings", (req, res) => {
+  const query = "SELECT * FROM bookings";
+  pool.query(query, (err, results) => {
     if (err) {
       console.error("Error fetching bookings: " + err.stack);
       res.status(500).send("Error fetching bookings");
       return;
     }
 
-    res.json(results);
-  })
-})
-
-
-
-
-
-
-
-
-
-
+    res.json(results.rows);
+  });
+});
 
 // Creates a Table for Bookings
 const ticketBookingsTable = `
-CREATE TABLE IF NOT EXISTS TicketBookings (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  UserId INT,
-  TimePurchased DATETIME,
-  Adults INT,
-  Seniors INT,
-  Children INT,
-  Price DECIMAL(10, 2),
-  FOREIGN KEY (UserId) REFERENCES Users(id)
+CREATE TABLE IF NOT EXISTS ticket_bookings (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  time_purchased TIMESTAMP,
+  adults INT,
+  seniors INT,
+  children INT,
+  price DECIMAL(10, 2),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 )
 `;
 
-db.query(ticketBookingsTable, (err) => {
+pool.query(ticketBookingsTable, (err) => {
   if (err) throw err;
   console.log('TicketBookings table created');
 });
 
-
-
 // Creates a Table for Reservations
 const hotelBookingsTable = `
-CREATE TABLE IF NOT EXISTS HotelBookings (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  UserId INT,
-  Rooms INT,
-  NumPeople INT,
-  Price DECIMAL(10, 2),
-  FOREIGN KEY (UserId) REFERENCES Users(id)
+CREATE TABLE IF NOT EXISTS hotel_bookings (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  rooms INT,
+  num_people INT,
+  price DECIMAL(10, 2),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 )
 `;
 
-db.query(hotelBookingsTable, (err) => {
+pool.query(hotelBookingsTable, (err) => {
   if (err) throw err;
   console.log('HotelBookings table created');
 });
-
